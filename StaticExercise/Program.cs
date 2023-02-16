f910ffe62ad74abd91c03f64e291795427e0e961
Justin Huth <Jayhuth96@gmail.com>
2023-02-16 14:39:26 -0600

    Completed the excercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+                Console.WriteLine("Enter Fahrenheit Temp:");
+                double fahrenheit = Convert.ToDouble(Console.ReadLine()); ;
+                double convertToCel = TempConverter.FahrenheitToCelsius(fahrenheit);
+                Console.WriteLine("The converted Temp is:" + convertToCel);
+                Console.ReadLine();
+
+                Console.WriteLine("Enter Celsius Temp:");
+                double celsius = Convert.ToDouble(Console.ReadLine()); ;
+                double convertToFahr = TempConverter.CelsiusToFahrenheit(celsius);
+                Console.WriteLine("the converted temp is:" + convertToFahr);
+                Console.ReadLine();
+
         }
+
     }
 }
